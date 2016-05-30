4f78932a60133e6fb2f1ba836cbfc9010204e367
Lara Martin <larikimc@gmail.com>
2016-05-30 16:39:20 +0200

    layout team A

---
+++ CourtCounter/app/src/main/java/eu/laramartin/courtcounter/MainActivity.java
@@ -0,0 +1,21 @@
+package eu.laramartin.courtcounter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    /**
+     * Displays the given score for Team A.
+     */
+    public void displayForTeamA(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_a_score);
+        scoreView.setText(String.valueOf(score));
+    }
+}
