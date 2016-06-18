124f2436cf9feb6d3f0c26dfcc3710d9f2a4c1f3
Lara Martin <larikimc@gmail.com>
2016-06-18 20:30:20 +0200

    name input, email intent

--- JustJava/app/src/main/java/com/example/android/justjava/MainActivity.java
+++ JustJava/app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -1,10 +1,18 @@
 package com.example.android.justjava;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.View;
 import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 import java.text.NumberFormat;
 
@@ -13,12 +21,20 @@ import java.text.NumberFormat;
  */
 public class MainActivity extends ActionBarActivity {
 
-    int quantity = 0;
+    int quantity = 1;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
     /**
@@ -29,35 +45,45 @@ public class MainActivity extends ActionBarActivity {
         boolean whippedCreamIsChecked = whippedCreamCheckbox.isChecked();
         CheckBox chocolateCheckbox = (CheckBox) findViewById(R.id.chocolate_checkbox);
         boolean chocolateIsChecked = chocolateCheckbox.isChecked();
-        int price = calculatePrice();
-        String priceMessage = createOrderSummary(price, whippedCreamIsChecked, chocolateIsChecked);
-        displayMessage(priceMessage);
+        int price = calculatePrice(whippedCreamIsChecked, chocolateIsChecked);
+        String customerName = customerNameInput();
+        createOrderSummary(price, whippedCreamIsChecked, chocolateIsChecked, customerName);
     }
 
-    private int calculatePrice() {
-        int price = quantity * 5;
+    private int calculatePrice(Boolean whippedCream, Boolean chocolate) {
+        //int cups = quantity * 5;
+        int toppings = 0;
+        if (whippedCream) {
+            toppings += 1;
+        }
+        if (chocolate) {
+            toppings += 2;
+        }
+        int price = quantity * (5 + toppings);
         return price;
     }
 
-    private String createOrderSummary(int price, boolean whippedCream, boolean chocolate){
-        String summary = "Name: Lara Martin" +
-                "\nAdd whipped cream? " + whippedCream +
-                "\nAdd chocolate? " + chocolate + 
-                "\nQuantity: " + quantity + "\n"+
-                "Total: $" + price + "\nThank you!";;
-        return summary;
-    }
-    /**
-     * This method displays the given text on the screen.
-     */
-    private void displayMessage(String message) {
-        TextView orderSummaryTextView = (TextView) findViewById(R.id.order_summary_text_view);
-        orderSummaryTextView.setText(message);
+    private String customerNameInput() {
+        EditText customerName = (EditText) findViewById(R.id.customer_name);
+        String name = customerName.getText().toString();
+        return name;
     }
 
-    /**
-     * This method displays the given quantity value on the screen.
-     */
+    private void createOrderSummary(int price, boolean whippedCream, boolean chocolate, String customerName) {
+        String summary = "Name: " + customerName +
+                "\nAdd whipped cream? " + whippedCream +
+                "\nAdd chocolate? " + chocolate +
+                "\nQuantity: " + quantity + "\n" +
+                "Total: $" + price + "\nThank you!";
+
+        composeEmail(summary);
+    }
+
+//    private void displayMessage(String message) {
+//        TextView orderSummaryTextView = (TextView) findViewById(R.id.order_summary_text_view);
+//        orderSummaryTextView.setText(message);
+//    }
+
     private void displayQuantity(int number) {
         TextView quantityTextView = (TextView) findViewById(
                 R.id.quantity_text_view);
@@ -72,13 +98,71 @@ public class MainActivity extends ActionBarActivity {
 //    }
 
     public void increment(View view) {
+        if (quantity == 100) {
+            Toast.makeText(this, "You cannot have more than 100 coffees", Toast.LENGTH_SHORT).show();
+            return;
+        }
         quantity = quantity + 1;
         displayQuantity(quantity);
     }
 
     public void decrement(View view) {
+        if (quantity == 1) {
+            Toast.makeText(this, "You cannot have less than 1 coffee", Toast.LENGTH_SHORT).show();
+            return;
+        }
         quantity = quantity - 1;
         displayQuantity(quantity);
     }
 
+    public void composeEmail(String bodyText) {
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:")); // only email apps should handle this
+        intent.putExtra(Intent.EXTRA_TEXT, bodyText);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        } else {
+            Toast.makeText(this, "There aren't email apps installed", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://com.example.android.justjava/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://com.example.android.justjava/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
+    }
 }
