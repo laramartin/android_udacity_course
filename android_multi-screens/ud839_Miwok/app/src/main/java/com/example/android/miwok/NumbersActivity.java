0cac976f60c462ab39fe233d834c6617376143cf
Lara Martin <larikimc@gmail.com>
2016-06-24 15:39:30 +0200

    numbers words in ArrayList

--- android_multi-screens/ud839_Miwok/app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ android_multi-screens/ud839_Miwok/app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.util.Log;
 
 import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -13,28 +14,26 @@ public class NumbersActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
 
-        String[] words = new String[10];
-        words[0] = "one";
-        words[1] = "two";
-        words[2] = "three";
-        words[3] = "four";
-        words[4] = "five";
-        words[5] = "six";
-        words[6] = "seven";
-        words[7] = "eight";
-        words[8] = "nine";
-        words[9] = "ten";
+        ArrayList<String> words = new ArrayList<String>();
 
-        Log.v("NumbersActivity", "Word at index 0: " + words[0]);
-        Log.v("NumbersActivity", "Word at index 1: " + words[1]);
-        Log.v("NumbersActivity", "Word at index 2: " + words[2]);
-        Log.v("NumbersActivity", "Word at index 3: " + words[3]);
-        Log.v("NumbersActivity", "Word at index 4: " + words[4]);
-        Log.v("NumbersActivity", "Word at index 5: " + words[5]);
-        Log.v("NumbersActivity", "Word at index 6: " + words[6]);
-        Log.v("NumbersActivity", "Word at index 7: " + words[7]);
-        Log.v("NumbersActivity", "Word at index 8: " + words[8]);
-        Log.v("NumbersActivity", "Word at index 9: " + words[9]);
+        words.add("one");
+        words.add("two");
+        words.add("three");
+        words.add("four");
+        words.add("five");
+        words.add("six");
+        words.add("seven");
+        words.add("eight");
+        words.add("nine");
+        words.add("ten");
+
+        Log.v("NumbersActivity", "words[0]: " + words.get(0));
+        Log.v("NumbersActivity", "words[2]: " + words.get(2));
+        Log.v("NumbersActivity", "words[4]: " + words.get(4));
+        Log.v("NumbersActivity", "words[6]: " + words.get(6));
+        Log.v("NumbersActivity", "words[8]: " + words.get(8));
+
+
     }
 
 }
