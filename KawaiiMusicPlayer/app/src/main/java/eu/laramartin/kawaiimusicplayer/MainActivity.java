569a32e178ffb15e825163ba86737a979a67cf63
Lara Martin <larikimc@gmail.com>
2016-07-02 11:48:51 +0200

    MediaPlayer usage example - play and pause

---
+++ KawaiiMusicPlayer/app/src/main/java/eu/laramartin/kawaiimusicplayer/MainActivity.java
@@ -0,0 +1,40 @@
+package eu.laramartin.kawaiimusicplayer;
+
+import android.media.MediaPlayer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final MediaPlayer song = MediaPlayer.create(this, R.raw.rolemusic_14_the_will);
+
+        Button playButton = (Button) this.findViewById(R.id.play);
+        Button pauseButton = (Button) this.findViewById(R.id.pause);
+
+        playButton.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                song.start();
+            }
+        });
+
+
+        pauseButton.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                song.pause();
+            }
+        });
+
+    }
+}
+
+
+
