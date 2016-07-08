7fa87ac596cd320950641c9cb14bf0527d0d3043
Lara Martin <larikimc@gmail.com>
2016-07-08 17:38:35 +0200

    parent activity up button

--- android_multi-screens/ud839_Miwok/app/src/main/java/com/example/android/miwok/ColorsActivity.java
+++ android_multi-screens/ud839_Miwok/app/src/main/java/com/example/android/miwok/ColorsActivity.java
@@ -3,8 +3,10 @@ package com.example.android.miwok;
 import android.content.Context;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -29,6 +31,7 @@ public class ColorsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.word_list);
+
 
         audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
 
@@ -78,6 +81,17 @@ public class ColorsActivity extends AppCompatActivity {
             }
         });
     }
+
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        switch (item.getItemId()) {
+//            // Respond to the action bar's Up/Home button
+//            case android.R.id.home:
+//                NavUtils.navigateUpFromSameTask(this);
+//                return true;
+//        }
+//        return super.onOptionsItemSelected(item);
+//    }
 
     private AudioManager.OnAudioFocusChangeListener audioFocusChangeListener =
             new AudioManager.OnAudioFocusChangeListener() {
