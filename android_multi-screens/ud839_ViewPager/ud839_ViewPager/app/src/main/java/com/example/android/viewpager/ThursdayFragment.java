16449fbefc7a9a61d97c2e6bd2ecb7d76b3d334c
Lara Martin <larikimc@gmail.com>
2016-07-15 13:24:54 +0200

    fragments example, added 2 fragments

---
+++ android_multi-screens/ud839_ViewPager/ud839_ViewPager/app/src/main/java/com/example/android/viewpager/ThursdayFragment.java
@@ -0,0 +1,29 @@
+package com.example.android.viewpager;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ThursdayFragment extends Fragment {
+
+
+    public ThursdayFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_thursday, container, false);
+    }
+
+}
