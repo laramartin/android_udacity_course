e40c2701e4742a6f9dacc538c9d3140f56bd77f9
Lara Martin <lara.martin.carretero@gmail.com>
2016-10-01 20:14:11 +0200

    URI constants for provider

--- android_data_storage/ud845-Pets/app/src/main/java/com/example/android/pets/data/PetContract.java
+++ android_data_storage/ud845-Pets/app/src/main/java/com/example/android/pets/data/PetContract.java
@@ -1,5 +1,6 @@
 package com.example.android.pets.data;
 
+import android.net.Uri;
 import android.provider.BaseColumns;
 
 /**
@@ -10,7 +11,14 @@ public class PetContract {
     private PetContract() {
     }
 
+
+    public static final String CONTENT_AUTHORITY = "com.example.android.pets";
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+    public static final String PATH_PETS = "pets";
+
     public static final class PetEntry implements BaseColumns {
+
+        public static final Uri CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI, PATH_PETS);
 
         public final static String TABLE_NAME = "pets";
 
