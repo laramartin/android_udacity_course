383627fd97106664fdb6a7efaaaa4ea2fd52f99a
Lara Martin <lara.martin.carretero@gmail.com>
2016-10-02 15:16:53 +0200

    hide delete option when adding new pet

--- android_data_storage/ud845-Pets/app/src/main/java/com/example/android/pets/EditorActivity.java
+++ android_data_storage/ud845-Pets/app/src/main/java/com/example/android/pets/EditorActivity.java
@@ -79,6 +79,7 @@ public class EditorActivity extends AppCompatActivity implements LoaderManager.L
         //If the intent doesn't contain a pet content URI, then we know we are creating a new pet.
         if (currentPetUri == null) {
             setTitle(getString(R.string.editor_activity_title_new_pet));
+            invalidateOptionsMenu();
         } else {
             // Otherwise this is an existing pet
             setTitle(getString(R.string.editor_activity_title_edit_pet));
@@ -248,6 +249,23 @@ public class EditorActivity extends AppCompatActivity implements LoaderManager.L
         // Inflate the menu options from the res/menu/menu_editor.xml file.
         // This adds menu items to the app bar.
         getMenuInflater().inflate(R.menu.menu_editor, menu);
+        return true;
+    }
+
+
+    /**
+     * This method is called after invalidateOptionsMenu(), so that the
+     * menu can be updated (some menu items can be hidden or made visible).
+    **/
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        super.onPrepareOptionsMenu(menu);
+        // If this is a new pet, hide the "Delete" menu item.
+        if (currentPetUri == null) {
+            MenuItem menuItem = menu.findItem(R.id.action_delete);
+            menuItem.setVisible(false);
+        }
         return true;
     }
 
